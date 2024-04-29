45d5ec79fe7f8dacac819b79cbd6d83938db62ec
Nadman Abdullah Bin Faisal <gusfaisna@student.gu.se>
2024-04-29 16:10:31 +0200

    #24 Updated notification class and thresholds

--- src/main/java/org/example/envirobaby/Notification.java
+++ src/main/java/org/example/envirobaby/Notification.java
@@ -5,60 +5,61 @@ import javafx.geometry.Pos;
 import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 // Source for the code:- https://javadoc.io/doc/org.controlsfx/controlsfx/8.40.16/org/controlsfx/control/Notifications.html
 public class Notification {
 
-    private DataProcessor processor = new DataProcessor();
+    private int lastNoiseAlert;
+    private double lastMaxTempAlert;
+    private double lastMinTempAlert;
+    private double lastMaxHumAlert;
+    private double lastMinHumAlert;
 
-    public void createNoiseNotification(String value, double threshold) {
-        if(processor.extractNumber(value) > threshold) {
-            Platform.runLater(() -> {
-                String notiMessage = "NOISE THRESHOLD CROSSED: " + processor.extractNumber(value) + " db";
-                String notiTitle = "Noise notification";
-                Notifications notifications = Notifications.create().title(notiTitle).text(notiMessage).graphic(null).hideAfter(Duration.seconds(7)).position(Pos.BOTTOM_RIGHT);
-                notifications.showWarning();
-            });
-        }
+    public Notification() {
+        this.lastNoiseAlert=0;
+        this.lastMaxTempAlert=0;
+        this.lastMinTempAlert=0;
+        this.lastMaxHumAlert=0;
+        this.lastMinHumAlert=0;
     }
 
-    public void createTempNotification(String value, double upperThreshold, double lowerThreshold) {
-        if(processor.extractDouble(value) > upperThreshold) {
-            Platform.runLater(() -> {
-                String notiMessaeg = "TEMPERATURE EXCEEDS THRESHOLD: " + processor.extractDouble(value) + " C";
-                String notiTitle = "Temperature notification";
-                Notifications notifications = Notifications.create().title(notiTitle).text(notiMessaeg).graphic(null).hideAfter(Duration.seconds(7)).position(Pos.BOTTOM_RIGHT);
-                notifications.showWarning();
-            });
-        } else if (processor.extractDouble(value) < lowerThreshold) {
-            Platform.runLater(() -> {
-                String notiMessage = "TEMPERATURE BELOW THRESHOLD: " + processor.extractDouble(value) + " C";
-                String notiTitle = "Temperature notification";
-                Notifications notifications = Notifications.create().title(notiTitle).text(notiMessage).graphic(null).hideAfter(Duration.seconds(7)).position(Pos.BOTTOM_RIGHT);
-                notifications.showWarning();
-            });
-        }
+    public void createNotification(String title, String message) {
+        Platform.runLater(() -> {
+            Notifications notifications = Notifications.create().title(title).text(message).graphic(null).hideAfter(Duration.seconds(7)).position(Pos.BOTTOM_RIGHT);
+            notifications.showWarning();
+        });
     }
 
-    public void createHumNotification(String value, double upperThreshold, double lowerThreshold) {
-        if(processor.extractDouble(value) > upperThreshold) {
-            Platform.runLater(() -> {
-                String notiMessaeg = "Humidity exceeded! " + value;
-                String notiTitle = "Humidity notification";
-                Notifications notifications = Notifications.create().title(notiTitle).text(notiMessaeg).graphic(null).hideAfter(Duration.seconds(7)).position(Pos.BOTTOM_RIGHT);
-                notifications.showWarning();
-            });
-        } else if (processor.extractDouble(value) < lowerThreshold) {
-            Platform.runLater(() -> {
-                String notiMessage = "Humidity too low! " + value;
-                String notiTitle = "Humidity notification";
-                Notifications notifications = Notifications.create().title(notiTitle).text(notiMessage).graphic(null).hideAfter(Duration.seconds(7)).position(Pos.BOTTOM_RIGHT);
-                notifications.showWarning();
-            });
-        }
+    public void setLastNoiseAlert(int lastNoiseAlert) {
+        this.lastNoiseAlert = lastNoiseAlert;
+    }
+    public void setLastMaxTempAlert(double lastMaxTempAlert) {
+        this.lastMaxTempAlert = lastMaxTempAlert;
+    }
+    public void setLastMinTempAlert(double lastMinTempAlert) {
+        this.lastMinTempAlert = lastMinTempAlert;
+    }
+    public void setLastMaxHumAlert(double lastMaxHumAlert) {
+        this.lastMaxHumAlert = lastMaxHumAlert;
+    }
+    public void setLastMinHumAlert(double lastMinHumAlert) {
+        this.lastMinHumAlert = lastMinHumAlert;
     }
 
+    public int getLastNoiseAlert() {
+        return lastNoiseAlert;
+    }
+    public double getLastMaxTempAlert() {
+        return lastMaxTempAlert;
+    }
+    public double getLastMinTempAlert() {
+        return lastMinTempAlert;
+    }
+    public double getLastMaxHumAlert() {
+        return lastMaxHumAlert;
+    }
+    public double getLastMinHumAlert() {
+        return lastMinHumAlert;
+    }
 }
---
+++ src/main/java/org/example/envirobaby/NotificationThreshold.java
@@ -0,0 +1,70 @@
+package org.example.envirobaby;
+
+public class NotificationThreshold {
+
+    private static final int DEFAULT_LOUD_THRESHOLD =90;
+    private static final double DEFAULT_CTEMP_UBOUND =25.00;
+    private static final double DEFAULT_CTEMP_LBOUND =18.00;
+    private static final double DEFAULT_HUM_UBOUND =60.00;
+    private static final double DEFAULT_HUM_LBOUND =30.00;
+
+
+    private int loudThreshold;
+    private double tempUpperBound;
+    private double tempLowerBound;
+    private double humUpperBound;
+    private double humLowerBound;
+
+
+
+    public NotificationThreshold(){
+        this.loudThreshold = DEFAULT_LOUD_THRESHOLD;
+        this.tempUpperBound = DEFAULT_CTEMP_UBOUND;
+        this.tempLowerBound = DEFAULT_CTEMP_LBOUND;
+        this.humUpperBound = DEFAULT_HUM_UBOUND;
+        this.humLowerBound = DEFAULT_HUM_LBOUND;
+    }
+
+
+    public void setLoudThreshold(int loudThreshold) {
+        this.loudThreshold = loudThreshold;
+    }
+    public void setTempUpperBound(double tempUpperBound) {
+        this.tempUpperBound = tempUpperBound;
+    }
+    public void setTempLowerBound(double tempLowerBound) {
+        this.tempLowerBound = tempLowerBound;
+    }
+    public void setHumUpperBound(double humUpperBound) {
+        this.humUpperBound = humUpperBound;
+    }
+    public void setHumLowerBound(double humLowerBound) {
+        this.humLowerBound = humLowerBound;
+    }
+
+    public int getLoudThreshold() {
+        return loudThreshold;
+    }
+    public double getTempUpperBound() {
+        return tempUpperBound;
+    }
+    public double getTempLowerBound() {
+        return tempLowerBound;
+    }
+
+    public double getHumUpperBound() {
+        return humUpperBound;
+    }
+
+    public double getHumLowerBound() {
+        return humLowerBound;
+    }
+
+    public void resetThresholds(){
+        this.loudThreshold = DEFAULT_LOUD_THRESHOLD;
+        this.tempUpperBound = DEFAULT_CTEMP_UBOUND;
+        this.tempLowerBound = DEFAULT_CTEMP_LBOUND;
+        this.humUpperBound = DEFAULT_HUM_UBOUND;
+        this.humLowerBound = DEFAULT_HUM_LBOUND;
+    }
+}
