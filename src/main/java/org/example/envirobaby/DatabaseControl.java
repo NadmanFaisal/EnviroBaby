39000f4332f6e24c6024b6e76dd746b45041fb18
Mesimaaria <alastalo@student.chalmers.se>
2024-05-18 12:34:15 +0200

    #29 add purpose and benefits

--- src/main/java/org/example/envirobaby/DatabaseControl.java
+++ src/main/java/org/example/envirobaby/DatabaseControl.java
@@ -3,9 +3,9 @@ package org.example.envirobaby;
 import java.sql.*;
 
 public class DatabaseControl {
-    String jdbcURL = "";
-    String username = "";
-    String password = "";
+    String jdbcURL = "jdbc:postgresql://65.19.141.67:5432/alevka_envirobaby";
+    String username = "alevka_envbUse";
+    String password = "Vzb3_783r";
     Connection connection;
     Statement statement;
 
